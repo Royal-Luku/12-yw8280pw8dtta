6bd43c4bb805d108678fcaeb47b2dc8b4079bce3
Royal Dwip <120001510+Royal-Luku@users.noreply.github.com>
2024-03-17 13:27:06 +0530

    Update Report-request.py

--- plugins/Report-request.py
+++ plugins/Report-request.py
@@ -1,11 +1,8 @@
 import asyncio
 import os
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import filters, enums, Client 
 
-#admin = InlineKeyboardMarkup([[InlineKeyboardButton("‼️ Mark as read ‼️", callback_data="close")]])
-
-@Client.on_message((filters.command(["request"]) | filters.regex("@request")) & filters.group)
+@Client.on_message(filters.command("request") & (filters.group | filters.private))
 async def report_user(bot, message):
     if message.reply_to_message:
         chat_id = message.chat.id
@@ -22,8 +19,7 @@ async def report_user(bot, message):
                     await reported_post.reply_text(
                         text=report,
                         chat_id=admin.user.id,
-                        disable_web_page_preview=True,
-                        #reply_markup=admin
+                        disable_web_page_preview=True
                     )
                     success = True
                 except:
@@ -31,5 +27,4 @@ async def report_user(bot, message):
             else: # Skipping Bots
                 pass
         if success:
-            await message.reply_text("**‼️ Movie Request Send Successfully ‼️")
-            
+            await message.reply_text("**‼️ Movie Request Send Successfully ‼️**")
